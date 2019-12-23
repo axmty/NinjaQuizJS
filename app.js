97f047f3552c39cbdbbb67622e3e89079a88317c
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-12-24 00:39:48 +0100

    Asynchronously load the questions.json file.

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
-const form = document.querySelector('.quiz-form');
-const resultDiv = document.querySelector('.result');
+// TODO: handle errors
+const loadQuiz = () => new Promise((resolve) => {
+  const xobj = new XMLHttpRequest();
 
-const animateScore = (score) => {
-  let scoreStep = 0;
-  const scoreSpan = resultDiv.querySelector('.score');
-  const scoreTimer = window.setInterval(() => {
-    scoreSpan.textContent = `${scoreStep}%`;
-    if (scoreStep === score) {
-      window.clearInterval(scoreTimer);
+  xobj.overrideMimeType('application/json');
+  xobj.open('GET', 'questions.json', true);
+
+  xobj.onload = () => new Promise(() => {
+    if (xobj.status === 200) {
+      resolve(JSON.parse(xobj.responseText));
     }
-    scoreStep += 1;
-  }, 20);
-};
+    return resolve({});
+  });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-  const score = userAnswers.reduce(
-    (prev, curr, i) => prev + (curr === correctAnswers[i] ? 25 : 0), 0,
-  );
-
-  resultDiv.classList.remove('d-none');
-  window.scrollTo(0, 0);
-  animateScore(score);
+  xobj.send();
 });
+
+(async () => {
+  const quiz = await loadQuiz();
+
+  const correctAnswers = ['B', 'B', 'B', 'B'];
+  const form = document.querySelector('.quiz-form');
+  const resultDiv = document.querySelector('.result');
+
+  const animateScore = (score) => {
+    let scoreStep = 0;
+    const scoreSpan = resultDiv.querySelector('.score');
+    const scoreTimer = window.setInterval(() => {
+      scoreSpan.textContent = `${scoreStep}%`;
+      if (scoreStep === score) {
+        window.clearInterval(scoreTimer);
+      }
+      scoreStep += 1;
+    }, 20);
+  };
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    const score = userAnswers.reduce(
+      (prev, curr, i) => prev + (curr === correctAnswers[i] ? 25 : 0), 0,
+    );
+
+    resultDiv.classList.remove('d-none');
+    window.scrollTo(0, 0);
+    animateScore(score);
+  });
+})();
