3f20668b576dc256b6af8be97dbc2f65dfeeed05
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-12-23 16:26:19 +0100

    Animate score when submitting.

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
 const resultDiv = document.querySelector('.result');
-const scoreSpan = document.querySelector('.score');
+
+const animateScore = (score) => {
+  let scoreStep = 0;
+  const scoreSpan = resultDiv.querySelector('.score');
+  const scoreTimer = window.setInterval(() => {
+    scoreSpan.textContent = `${scoreStep}%`;
+    if (scoreStep === score) {
+      window.clearInterval(scoreTimer);
+    }
+    scoreStep += 1;
+  }, 20);
+};
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-  const score = userAnswers
-    .reduce((prev, curr, i) => prev + (curr === correctAnswers[i] ? 25 : 0), 0);
-
-  scoreSpan.textContent = `${score}%`;
+  const score = userAnswers.reduce(
+    (prev, curr, i) => prev + (curr === correctAnswers[i] ? 25 : 0), 0,
+  );
 
   resultDiv.classList.remove('d-none');
+  window.scrollTo(0, 0);
+  animateScore(score);
 });
