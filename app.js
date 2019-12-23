c0a8522e335e17d4099a8122b159b1b4b794c68d
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-12-23 15:23:11 +0100

    Edit tab size. Add score display on the page.

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const resultDiv = document.querySelector('.result');
+const scoreSpan = document.querySelector('.score');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let score = 0;
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+  const score = userAnswers
+    .reduce((prev, curr, i) => prev + (curr === correctAnswers[i] ? 25 : 0), 0);
 
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 25;
-    }
-  });
+  scoreSpan.textContent = `${score}%`;
 
-  console.log(score);
+  resultDiv.classList.remove('d-none');
 });
